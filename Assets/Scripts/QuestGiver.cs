76f3b406564adffe7ac9f9cf1537f619bb24ff76
MRbubbelgum <malte.gundhus@gmail.com>
2023-09-08 17:58:56 +0200

    E button

--- Assets/Scripts/QuestGiver.cs
+++ Assets/Scripts/QuestGiver.cs
@@ -6,14 +6,29 @@ public class QuestGiver : MonoBehaviour
 {
     [SerializeField] private GameObject textPopUp;
     [SerializeField] private AudioClip hmmSound;
+    [SerializeField] private GameObject pressEText;
     private AudioSource audioSource;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("MainCharacter"))
+        {
+            if (pressEText != null && Input.GetKey(KeyCode.E))
+            {
+                textPopUp.SetActive(true);
+                pressEText.SetActive(false);
+            }
+                
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("MainCharacter"))
         {
-            textPopUp.SetActive(true);
+            pressEText.SetActive(true);
             audioSource = GetComponent<AudioSource>();
             audioSource.PlayOneShot(hmmSound, 0.5f);
+            
         }  
     }
     private void OnTriggerExit2D(Collider2D other)
@@ -28,6 +43,7 @@ public class QuestGiver : MonoBehaviour
     public void WaitUntilInactive()
     {
         textPopUp.SetActive(false);
+        pressEText.SetActive(false);
     }
 
     
