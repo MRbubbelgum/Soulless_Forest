05cfff3d9151641d7cbdcaff15ca84c74a1da02a
MRbubbelgum <malte.gundhus@gmail.com>
2023-09-13 21:45:25 +0200

    Update Lever.cs

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.gameObject.CompareTag("MainCharacter"))
+        {
+        }
     }
 }
