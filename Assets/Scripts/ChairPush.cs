1e4a8fb0f9a5937730f4e4d559c920ca8381e37a
MRbubbelgum <malte.gundhus@gmail.com>
2023-09-14 23:26:28 +0200

    Quest update level 2 & sound update level 1

--- Assets/Scripts/ChairPush.cs
+++ Assets/Scripts/ChairPush.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ChairPush : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
+    private AudioSource audioSource;
+    private bool isPlayingAudio = false;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MainCharacter") && Mathf.Abs(rb.velocity.x) > 0.1f)
+        {
+            if (isPlayingAudio == false)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MainCharacter") && Mathf.Abs(rb.velocity.x) <= 0f)
+        {
+            audioSource.Stop();
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MainCharacter"))
+        {
+            audioSource.Stop();
+        }
     }
 }
